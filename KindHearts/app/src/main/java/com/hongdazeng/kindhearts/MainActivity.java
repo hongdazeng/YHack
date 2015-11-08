ee7751411e76a2523aab776a7609ba4990d6db0d
bnehoran <bnehoran@hotmail.com>
2015-11-07 22:07:33 -0800

    Reading and writing to server data

--- KindHearts/app/src/main/java/com/hongdazeng/kindhearts/MainActivity.java
+++ KindHearts/app/src/main/java/com/hongdazeng/kindhearts/MainActivity.java
@@ -10,15 +10,43 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import com.microsoft.windowsazure.mobileservices.*;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.MalformedURLException;
+
+//added and mostly extraneous
+
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.table.TableOperationCallback;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText name;
     private EditText helpReason;
     private Button buttonHelp;
     private Button buttonRefresh;
     private Switch statusToggle;
+    private MobileServiceClient mClient;
 
+    Help someHelp;
+
+
+    public class Help {
+        public String Id;
+        public String Name;
+        public String Request;
+    }
+
+    public class User {
+        public String Id;
+        public String Name;
+        public int Long;
+        public int Lat;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,23 +70,128 @@ public class MainActivity extends AppCompatActivity {
         this.buttonRefresh = (Button) findViewById(R.id.button2);
         this.statusToggle = (Switch) findViewById(R.id.switch1);
 
+        buttonHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Help item = new Help();
+                item.Name = name.getText().toString();
+                item.Request = helpReason.getText().toString();
+                mClient.getTable(Help.class).insert(item, new TableOperationCallback<Help>() {
+                    public void onCompleted(Help entity, Exception exception, ServiceFilterResponse response) {
+                        if (exception == null) {
+                            // Insert succeeded
+                        } else {
+                            // Insert failed
+                        }
+                    }
+                });
+            }
+        });
+
+        buttonRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (statusToggle.isChecked()) {
+                    //add user to helpers table
+                    User user = new User();
+                    user.Name = name.getText().toString();
+                    user.Long = 1;
+                    user.Lat = 2;
+                    mClient.getTable(User.class).insert(user, new TableOperationCallback<User>() {
+                        public void onCompleted(User entity, Exception exception, ServiceFilterResponse response) {
+                            if (exception == null) {
+                                // Insert succeeded
+                            } else {
+                                // Insert failed
+                            }
+                        }
+                    });
+
+                    //dislpay users who need help
+                    new AsyncTask<Void, Void, MobileServiceList<Help>>() {
+
+                        MobileServiceList<Help> result;
+
+                        @Override
+                        protected MobileServiceList<Help> doInBackground(Void... params) {
+                            try {
+
+                                result = mClient.getTable(Help.class).execute().get();
+                                runOnUiThread(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        String body = "";
+                                        for (int i = 0; i < 5 && i < result.size(); i++) {
+                                            Help help = result.get(i);
+                                            body += help.Name + ": " + help.Request + "\n";
+                                        }
+                                        createAndShowDialog(new Exception(body), "Help Requests");
+
+//                                        getHelp(result.get(0));
+//                                        mAdapter.clear();
+//                                        for (ToDoItem item : result) {
+//                                            mAdapter.add(item);
+//                                        }
+                                    }
+                                });
+                            } catch (Exception exception) {
+                                createAndShowDialog(exception, "Error");
+                            }
+                            return result;
+                        }
+                    }.execute();
+
+                } else {
+                    //not available
+                }
+            }
+
+
+        });
+
+        try {
+            mClient = new MobileServiceClient(
+                    "https://kh-helper.azure-mobile.net/",
+                    "mjFiRdonjvSmmGdVUaEtMcLGoNCkrq37",
+                    this
+            );
+
+        } catch (MalformedURLException e) {
+            createAndShowDialog(new Exception("There was an error creating the Mobile Service. Verify the URL"), "Error");
+        } catch (Exception e) {
+            createAndShowDialog(e, "Error");
+        }
+
+        createAndShowDialog(new Exception("Welcome to KindHearts!"), "KindHearts");
 
         // User newuser = User(getname(), getmessage(), help, range)
         // Client newClient = new Client(User)
         // newClient.getserver()
     }
 
+    private void yell(String message) {
+        createAndShowDialog(new Exception(message), message);
+    }
+
     public String getName() {
         return this.name != null ? name.getText().toString() : "John";
+    }
+
+    public void getHelp(Help a){
+        someHelp = a;
     }
 
     public String getMessage() {
         return this.helpReason != null ? helpReason.getText().toString() : "need help";
     }
 
+    //redundant
     public void onClick(View v) {
+
         if (v == buttonHelp) {
             // call server update with change
+
         } else if (v == buttonRefresh) {
             // call sever update with change
         } else if (v == statusToggle) {
@@ -89,5 +222,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Creates a dialog and shows it
+     *
+     * @param exception
+     *            The exception to show in the dialog
+     * @param title
+     *            The dialog title
+     */
+    private void createAndShowDialogFromTask(final Exception exception, String title) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowDialog(exception, "Error");
+            }
+        });
+    }
+
+    /**
+     * Creates a dialog and shows it
+     *
+     * @param exception
+     *            The exception to show in the dialog
+     * @param title
+     *            The dialog title
+     */
+    private void createAndShowDialog(Exception exception, String title) {
+        Throwable ex = exception;
+        if(exception.getCause() != null){
+            ex = exception.getCause();
+        }
+        createAndShowDialog(ex.getMessage(), title);
+    }
+
+    /**
+     * Creates a dialog and shows it
+     *
+     * @param message
+     *            The dialog message
+     * @param title
+     *            The dialog title
+     */
+    private void createAndShowDialog(final String message, final String title) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(message);
+        builder.setTitle(title);
+        builder.create().show();
     }
 }
